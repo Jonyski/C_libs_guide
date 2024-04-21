26eab200d545b2c8b3d78e5d336def85d94ddd64
jonyski <geleiaeavela@gmail.com>
2024-04-20 23:43:11 -0300

    adds ctype.h testing

--- header_testing.c
+++ header_testing.c
@@ -4,9 +4,8 @@
 / IME-USP: https://www.ime.usp.br/~pf/algorithms/appendices/libraries.html
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-
-
 // INCLUDING ALL STANDARD LIBRARY HEADERS
+
 #include <assert.h> // macro that compares argument to zero
 #include <complex.h> // complex numbers arithmetics
 #include <ctype.h> // functions to determine the type contained in character data
@@ -76,7 +75,7 @@ void testWCtypeH();
 // its respective function and call it in main
 // (and ofc, modify the functions as you please, experiment!)
 int main(int argc, char const *argv[]) {
-	testComplexH();
+	testCtypeH();
 	return 0;
 }
 
@@ -119,13 +118,56 @@ void testComplexH() {
 	printf("double complex = %.2lf + %.2lfi\n", creal(doubleComplex), cimag(doubleComplex));
 	printf("long double complex = %.5Lf + %.5Lfi\n", creall(ldoubleComplex), cimagl(ldoubleComplex));
 
+	// some functions that manipulate complex numbers
 	printf("\nabsolute (modulus) value of a complex: %lf\n", cabs(doubleComplex));
 	printf("phase angle of a complex: %lf\n", carg(doubleComplex));
 	printf("conjulgate of a complex: %lf + %lfi\n", creal(conj(doubleComplex)), cimag(conj(doubleComplex)));
 	printf("projection of a complex on Riemann sphere: %lf + %lfi\n", creal(cproj(doubleComplex)), cimag(cproj(doubleComplex)));
 
+	// complex versions of the pow() and sqrt() functions
 	printf("\na complex to the power of 2: %lf + %lfi\n", creal(cpow(doubleComplex, 2)), cimag(cpow(doubleComplex, 2)));
 	printf("the square root of a complex: %lf + %lfi\n", creal(csqrt(doubleComplex)), cimag(csqrt(doubleComplex)));
 
+	// there are many other functions in the complex library, but I think you got the point,
+	// and I'm not math-pilled enough to know what exactly the other ones do. But if you want to know
+	// more, access https://en.cppreference.com/w/c/numeric/complex
+}
 
+void testCtypeH() {
+	char A = 'A';
+	char num1 = '1';
+	char dot = '.';
+	char space = ' ';
+
+	// isalnum() checks if the char is alpha-numeric
+	printf("%c is %salphanumeric\n", dot, isalnum(dot) ? "" : "not ");
+	// isalpha() checks if the char is in the alphabet
+	printf("%c is %sin the alphabet\n", A, isalpha(A) ? "" : "not ");
+	// islower() checks if the char is lowercase
+	printf("%c is %slowercase\n", A, islower(A) ? "" : "not ");
+	// isupper() checks if the char is uppercase
+	printf("%c is %suppercase\n", A, isupper(A) ? "" : "not ");
+	// isdigit() checks if the char is a digit
+	printf("%c is %sa digit\n", num1, isdigit(num1) ? "" : "not ");
+	// isxdigit() checks if the char is a hex digit
+	printf("%c is %sa hex digit\n", A, isxdigit(A) ? "" : "not ");
+	// iscntrl() checks if the char is a control character
+	printf("space is %sa control character\n", iscntrl(space) ? "" : "not ");
+	// isgraph() checks if the char has a graphical representation
+	printf("%c has %sa graphical representation\n", num1, isgraph(num1) ? "" : "not ");
+	// isspace() checks if the char is the space char
+	printf("%c is %sthe space character\n", dot, isspace(dot) ? "" : "not ");
+	// isblank() checks if the char is the space or tab char
+	printf("space is %sa blank character\n", isblank(space) ? "" : "not ");
+	// isprint() checks if the char is printable
+	printf("%c is %sprintable\n", A, isprint(A) ? "" : "not ");
+	// ispunct() checks if the char is a punctuation
+	printf("%c is %sa punctuation\n", dot, ispunct(dot) ? "" : "not ");
+
+	char lowercaseD = 'd';
+	char uppercaseD = 'D';
+
+	// you can change a lowercase char to uppercase and vice-versa
+	printf("\n%c\n", tolower(uppercaseD));
+	printf("%c\n", toupper(lowercaseD));
 }
